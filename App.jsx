9c5d2360839c3726b088e84154692bde1bba1076
itsadis <137875735+itsadis@users.noreply.github.com>
2024-10-31 09:13:18 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Button = (props) => {
+  const { children = "...", variant = "bg-black" } = props;
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+      {children}
+    </button>
+  );
+};
+
+function App() {
+  return (
+    <div className="flex justify-center bg-cyan-100 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-green-300">Login</Button>
+        <Button variant="bg-blue-400">Logout</Button>
+        <Button></Button>
+        <Button variant="bg-red-500">Submit</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
